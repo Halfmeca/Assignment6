2ca609e9910f521f514d45733797ffb47b11dde7
Halfmeca <Halfmeca@yahoo.com>
2019-04-11 11:53:10 -0700

    Begining to add the decoration factory
    Just adding the basic frame work for decorations

---
+++ src/DecoratorFactory/Decorator.java
@@ -0,0 +1,9 @@
+package DecoratorFactory;
+
+import java.util.List;
+
+public class Decorator {
+    
+   private  List <String> deocrations;
+    
+}
--- src/Factory/Car.java
+++ src/Factory/Car.java
@@ -1,13 +1,30 @@
 package Factory;
 
+import java.util.List;
+
 public class Car {
     
     private CarTypes type;
-
+    private List<String> tradeMods;
+    
     public Car(CarTypes type) {
-       this.type = type;
+        this.type = type;
+    }
+    
+    public CarTypes getCarType() {
+        return type;
+    }
+    
+    public void applyTrades(List<String> traits)
+    {
+        tradeMods = traits;
     }
     
     
+    public List<String> getTradeMods()
+    {
+        
+        return tradeMods;
+    }
     
 }
