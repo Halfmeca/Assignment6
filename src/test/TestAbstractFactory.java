261719df70420d8b2502f6ec8984b627c1e6195c
Halfmeca <Halfmeca@yahoo.com>
2019-04-15 22:54:09 -0700

    Added somemore test classes
    Added Several new tests mostly for the decorator class and the Strategy/ObserverClass

--- src/test/TestAbstractFactory.java
+++ src/test/TestAbstractFactory.java
@@ -2,15 +2,22 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import factoryabstract.Car;
 import factoryabstract.CarType;
 import factoryabstract.Factory;
 import factoryabstract.FactoryMaker;
+import factoryabstract.HybridCar;
+import factoryabstract.SportCar;
+import factoryabstract.TruckCar;
 
 public class TestAbstractFactory {
     
@@ -41,6 +48,55 @@ public class TestAbstractFactory {
         assertEquals(newFactory.getTrade().get(0), "TEST2");
         newFactory = newFactoryMaker.getFactory(CarType.TRUCK, "TEST3");
         assertEquals(newFactory.getTrade().get(0), "TEST3");
+        
+    }
+    
+    @Test
+    public void testFactoriesCreateCar() {
+        
+        FactoryMaker newFactoryMaker = new FactoryMaker();
+        
+        Factory newFactory = newFactoryMaker.getFactory(CarType.HYBRID, "TEST1");
+        
+        Car outputCar;
+        outputCar = newFactory.createCar();
+        
+        HybridCar testHybridCar;
+        testHybridCar = new HybridCar();
+        List<String> trades = new ArrayList<>();
+        trades.add("TEST1");
+        testHybridCar.setTrade(trades);
+        testHybridCar.setType(CarType.HYBRID);
+        
+        assertEquals(testHybridCar.getTrade(), outputCar.getTrade());
+        assertEquals(testHybridCar.getType(), outputCar.getType());
+        
+        trades.clear();
+        trades.add("TEST2");
+        newFactory = newFactoryMaker.getFactory(CarType.SPORT, "TEST2");
+        outputCar = newFactory.createCar();
+        SportCar testSportCar;
+        testSportCar = new SportCar();
+        testSportCar.setTrade(trades);
+        testSportCar.setType(CarType.SPORT);
+        
+        assertEquals(testSportCar.getTrade(), outputCar.getTrade());
+        assertEquals(testSportCar.getType(), outputCar.getType());
+        
+        trades.clear();
+        trades.add("TEST3");
+        
+        newFactory = newFactoryMaker.getFactory(CarType.TRUCK, "TEST3");
+        outputCar = newFactory.createCar();
+        TruckCar testTruckCar;
+        testTruckCar = new TruckCar();
+        testTruckCar.setTrade(trades);
+        testTruckCar.setType(CarType.TRUCK);
+        
+        assertEquals(testTruckCar.getTrade(), outputCar.getTrade());
+        assertEquals(testTruckCar.getType(), outputCar.getType());
+        
+        
         
         
     }
---
+++ src/test/TestStrategyObserver.java
@@ -0,0 +1,72 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import strategy.BadPaying;
+import strategy.EmployeePayStrat;
+import strategy.GoodPaying;
+import strategy.HorriblePaying;
+import strategy.OkayPaying;
+import strategy.WellPaying;
+
+public class TestStrategyObserver {
+    
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+    
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() throws Exception {
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+    @Test
+    public void testStates() {
+        //This test is jsst to make sure everything creates ok 
+        EmployeePayStrat testStrat = new GoodPaying();
+        assertEquals(testStrat.getBenifits(), "Health Care");
+        assertEquals(testStrat.getPayStrat(), "Direct Transfer");
+        assertEquals(testStrat.getPayTime(), 4);
+        assertEquals(testStrat.getPayValue(), 8);
+        
+        testStrat = new HorriblePaying();
+        assertEquals(testStrat.getPayStrat(), "Under the table");
+        assertEquals(testStrat.getBenifits(), "15 min break");
+        assertEquals(testStrat.getPayTime(), 8);
+        assertEquals(testStrat.getPayValue(), 4);
+        
+        
+        testStrat = new OkayPaying();
+        assertEquals(testStrat.getPayStrat(), "Check");
+        assertEquals(testStrat.getBenifits(), "Lunch Break");
+        assertEquals(testStrat.getPayTime(), 6);
+        assertEquals(testStrat.getPayValue(), 6);
+        
+        testStrat = new WellPaying();
+        assertEquals(testStrat.getPayStrat(), "Big Bag of money");
+        assertEquals(testStrat.getBenifits(), "Vacation Days");
+        assertEquals(testStrat.getPayTime(), 2);
+        assertEquals(testStrat.getPayValue(), 10);
+        
+        testStrat = new BadPaying();
+        assertEquals(testStrat.getBenifits(), "None");
+        assertEquals(testStrat.getPayStrat(), "Out a window");
+        assertEquals(testStrat.getPayTime(), 10);
+        assertEquals(testStrat.getPayValue(), 2);
+        
+    }
+    
+}
